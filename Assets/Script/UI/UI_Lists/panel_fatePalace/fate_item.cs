5a6380eb61b7507e531bdfe2281cf148ebd6ca39
lc_zhangwofencun <xiangsongtcm@126.com>
2025-05-15 17:09:18 +0800

    no message

--- Assets/Script/UI/UI_Lists/panel_fatePalace/fate_item.cs
+++ Assets/Script/UI/UI_Lists/panel_fatePalace/fate_item.cs
@@ -9,6 +9,7 @@ public class fate_item : Base_Mono
 
     private Image item_image;
     private Text quantity,number;
+
     private void Awake()
     {
         item_image=Find<Image>("item_image");
@@ -16,11 +17,12 @@ public class fate_item : Base_Mono
         number = Find<Text>("number");
     }
 
-    public void Init((string, int, int, int, int) data)//名字，奖励数量，抽奖次数，抽奖次数上限
+    public void Init((string, int, int, int, int) data, int num)//名字，奖励数量，抽奖次数，抽奖次数上限，权重
     {
         item_image.sprite = UI.UI_Manager.I.GetEquipSprite("icon/", data.Item1);
         quantity.text = data.Item3.ToString();
-        number.text = data.Item4.ToString() + "/" + data.Item4.ToString();
+        
+        number.text = num + "/" + data.Item4.ToString();
     }
 
 
