bc23fe1bb6b77e0989771af5312319294a92a1bf
hw <916011085@qq.com>
2025-07-26 11:20:50 +0800

    新增洗练功能

--- Assets/Script/Framework/Frame_Work/tool_Categoryt.cs
+++ Assets/Script/Framework/Frame_Work/tool_Categoryt.cs
@@ -331,19 +331,97 @@ public class tool_Categoryt : MonoBehaviour
                 continue;  
             }
         }
-        string user_value = bag_name;
-        //强化等级
-        user_value += " " + 0;
-        //品质
         int quality = Quality();
         if (lv != -1) quality = lv;
+        bag.user_value = Obtain_Equip(bag, 0, quality);
+        //string user_value = bag_name;
+        ////强化等级
+        //user_value += " " + 0;
+        ////品质
+        //user_value += " " + quality;
+        //if (quality > 0)
+        //{
+        //    List<(int, int)> list = new List<(int, int)>();
+        //    //随机属性 1物攻下 2物攻上 3魔攻下 4魔攻上 5物防  6魔防 7生命8魔法
+        //    int base_quality = Random.Range(1, 9);
+        //    int base_quality_value = Random.Range(bag.need_lv+5, bag.need_lv * 2 + 10);
+        //    if (base_quality > 6) base_quality_value *= 10;
+        //    list.Add((base_quality, base_quality_value));
+        //    if (quality > 1)
+        //    {
+        //        base_quality = Random.Range(8, 15);
+        //        base_quality_value = Random.Range(3, bag.need_lv / 2 + 6);
+        //        if (base_quality <= 9) base_quality_value *= 10;
+        //        list.Add((base_quality, base_quality_value));
+        //        if (quality > 2)
+        //        {
+        //            base_quality = Random.Range(8, 15);
+        //            base_quality_value = Random.Range(3, bag.need_lv / 2 + 6);
+        //            if (base_quality <= 9) base_quality_value *= 10;
+        //            list.Add((base_quality, base_quality_value));
+        //            if( quality > 3)
+        //            {
+        //                base_quality = Random.Range(19, 30);
+        //                base_quality_value = Random.Range(3, 6);
+        //                list.Add((base_quality, base_quality_value));
+        //                if (quality > 4)
+        //                {
+        //                    base_quality = Random.Range(19, 30);
+        //                    base_quality_value = Random.Range(3, 6);
+        //                    list.Add((base_quality, base_quality_value));
+        //                    if (quality > 5)
+        //                    {
+        //                        base_quality = Random.Range(19, 30);
+        //                        base_quality_value = Random.Range(3, 6);
+        //                        list.Add((base_quality, base_quality_value));
+        //                        if (quality > 6)
+        //                        {
+        //                            //五行
+        //                            base_quality = Random.Range(30, 35);
+        //                            base_quality_value = Random.Range(3, bag.need_lv / 2 + 6);
+        //                            list.Add((base_quality, base_quality_value));
+        //                        }
+        //                    }
+        //                }
+        //            }
+        //        } 
+        //    }
+        //    string type = "", value = "";
+        //    int index = 0;
+        //    foreach (var item in list)
+        //    {
+        //        index++;
+        //        type += item.Item1 + (index == list.Count ? "" : ",");
+        //        value += item.Item2 + (index == list.Count ? "" : ",");
+        //    }
+        //    user_value += " " + type + " " + value;
+        //}
+        ////是否可以锁定
+        //user_value += " " + 0;
+        //bag.user_value = user_value;
+        return bag;
+    }
+    /// <summary>
+    /// 洗练装备属性
+    /// </summary>
+    /// <param name="bag">物品基础信息</param>
+    /// <param name="lv">强化等级</param>
+    /// <param name="quality">装备品质</param>
+    /// <param name="islock">是否锁定</param>
+    /// <returns></returns>
+    public static string Obtain_Equip(Bag_Base_VO bag,int lv, int quality,int islock=0)
+    {
+        string user_value = bag.Name;
+        //强化等级
+        user_value += " " + lv;
+        //品质
         user_value += " " + quality;
         if (quality > 0)
         {
             List<(int, int)> list = new List<(int, int)>();
             //随机属性 1物攻下 2物攻上 3魔攻下 4魔攻上 5物防  6魔防 7生命8魔法
             int base_quality = Random.Range(1, 9);
-            int base_quality_value = Random.Range(bag.need_lv+5, bag.need_lv * 2 + 10);
+            int base_quality_value = Random.Range(bag.need_lv + 5, bag.need_lv * 2 + 10);
             if (base_quality > 6) base_quality_value *= 10;
             list.Add((base_quality, base_quality_value));
             if (quality > 1)
@@ -358,7 +436,7 @@ public class tool_Categoryt : MonoBehaviour
                     base_quality_value = Random.Range(3, bag.need_lv / 2 + 6);
                     if (base_quality <= 9) base_quality_value *= 10;
                     list.Add((base_quality, base_quality_value));
-                    if( quality > 3)
+                    if (quality > 3)
                     {
                         base_quality = Random.Range(19, 30);
                         base_quality_value = Random.Range(3, 6);
@@ -383,7 +461,7 @@ public class tool_Categoryt : MonoBehaviour
                             }
                         }
                     }
-                } 
+                }
             }
             string type = "", value = "";
             int index = 0;
@@ -396,9 +474,8 @@ public class tool_Categoryt : MonoBehaviour
             user_value += " " + type + " " + value;
         }
         //是否可以锁定
-        user_value += " " + 0;
-        bag.user_value = user_value;
-        return bag;
+        user_value += " " + islock;
+        return user_value;
     }
     public static Bag_Base_VO crate_equip(string bag_name, bool boss=false, bool isSuperlative=false, user_map_vo map = null)
     {
