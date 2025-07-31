c497e288a76f39529eebc80849805a78a680e880
miemie <1761181057@qq.com>
2025-07-31 15:59:00 +0800

    no message

--- Assets/Script/UI/UI_Lists/panel_login/panel_login.cs
+++ Assets/Script/UI/UI_Lists/panel_login/panel_login.cs
@@ -393,14 +393,14 @@ namespace MVC
           
 #if UNITY_EDITOR
             #region ios区 
-            SumSave.uid = "DSFSDFSDFSDF3dssssssd"; // 32ac681e5ca845dc9eda67b30a36f6fa
+            SumSave.uid = "DSFSDFSDFSDF3dssssssduu"; // 32ac681e5ca845dc9eda67b30a36f6fa
             //SumSave.uid = "e8735c45680048f8a3494d7107a9e5a5";//都做了土（ip） 
             //SumSave.uid = "399be5eb64f84f01b2e4817bba58a4e3";//殇璃（ip） 
             //SumSave.uid = "275193c861334549a4610b81bb9a6411";//都做了土（ip）
             SumSave.par = -1;
             #endregion
             #region 安卓区
-            SumSave.uid = "DSFSDFSDFSDF3";//
+            //SumSave.uid = "DSFSDFSDFSDF3";//
 
             //SumSave.uid = "0907c455977b4b6bb8f2eb13a7348430";//自古圣贤皆寂寞(Tap id)
 
